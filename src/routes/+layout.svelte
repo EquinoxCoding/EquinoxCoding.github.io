72f697eac6fc25b02680b8e40de4f49e71ea1648
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2023-08-30 00:21:48 -0400

    messing with background

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,18 +6,14 @@
 <div class="h-fit">
     <div class="w-full h-fit isolate absolute top-0">
         <div class="bg-noise w-full h-full absolute -z-10" />
-        <div class="mix-blend-darken absolute -z-10 w-full h-full bg-secondary" />
+        <div class="mix-blend-multiply absolute -z-10 w-full h-full bg-secondary" />
         <slot />
     </div>
 </div>
 
 <style>
     .bg-noise {
-        background: radial-gradient(
-                circle at 300vw 200vh,
-                rgb(var(--color-bg) / 0.1),
-                rgb(var(--color-bg))
-            ),
+        background: radial-gradient(circle 180vw at 190vw 150vh, rgb(var(--color-bg) / 0.1), rgb(var(--color-bg))),
             url('$lib/grain.svg');
         filter: contrast(900%) brightness(80%) grayscale(0.9);
     }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,12 +5,23 @@
 </script>
 
 <div class="h-fit max-w-full">
-    <div class="h-screen flex content-center items-end p-12" bind:clientHeight={navbarAppearY}>
+    <div
+        class="h-screen flex content-center items-end p-12"
+        bind:clientHeight={navbarAppearY}
+        id="title"
+    >
         <h1 class="text-primary text-xl">Winter Hacks Coming Soon</h1>
     </div>
-    <div class="">
-        <Navbar appearY={navbarAppearY}/>
+    <main>
+        <Navbar appearY={navbarAppearY} />
+        <div class="h-screen" id="about" />
+        <div class="h-screen" id="events" />
+        <div class="h-screen" id="team" />
+        <div class="h-screen" id="contact" />
         <div class="h-screen" />
         <div class="h-screen" />
-    </div>
+        <div class="h-screen" />
+        <div class="h-screen" />
+        <div class="h-screen" />
+    </main>
 </div>
