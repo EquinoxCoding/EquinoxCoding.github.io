684e3760be43c2bed3b2412e2e8135d224bee35e
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2023-08-30 02:17:09 -0400

    winterhacks coming soon

---
+++ src/lib/Snowflake.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+    export let size: number;
+    export let x: number;
+    export let y: number;
+    export let delay = 1;
+
+    let clazz = '';
+    export { clazz as class };
+</script>
+
+<div
+    class="snowflake-wrapper absolute w-fit h-fit text-primary {clazz}"
+    style="left:{x}vw; top:{y}vh; animation-delay: {delay + 100}ms"
+>
+    <i
+        style="font-size: {size}px; animation-delay: {delay}ms;"
+        class="snowflake opacity-50 absolute">*</i
+    >
+</div>
+
+<style>
+    .snowflake {
+        transform: rotate(-15deg);
+        animation: tilt ease-in-out infinite alternate-reverse 5s;
+    }
+
+    .snowflake-wrapper {
+        animation: wobble ease-in-out infinite alternate-reverse 5s;
+        transform: translateY(10px);
+    }
+
+    @keyframes wobble {
+        from {
+            transform: translateY(-10px);
+        }
+        to {
+            transform: translateY(10px);
+        }
+    }
+
+    @keyframes tilt {
+        from {
+            transform: rotate(15deg);
+        }
+        to {
+            transform: rotate(-15deg);
+        }
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,46 @@
 <script lang="ts">
     import Navbar from '$lib/Navbar.svelte';
+    import Snowflake from '$lib/Snowflake.svelte';
 
     let navbarAppearY: number;
+
+    interface SnowflakeData {
+        size: number;
+        x: number;
+        y: number;
+        delay: number;
+    }
+
+    let numSnowflakes = 20;
+    let snowflakes = new Array<SnowflakeData>(10);
+    for (let i = 0; i < numSnowflakes; i++) {
+        snowflakes[i] = {
+            size: 85 + Math.random() * 30,
+            x: Math.random() * 120 - 10,
+            y: Math.random() * 120 - 10,
+            delay: Math.random() * 3000
+        };
+    }
 </script>
 
 <div class="h-fit max-w-full">
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
     <div
-        class="h-screen flex content-center items-end p-12"
+        class="h-screen flex-col flex justify-center items-center p-12 overflow-clip"
         bind:clientHeight={navbarAppearY}
         id="title"
     >
-        <h1 class="text-primary text-xl">Winter Hacks Coming Soon</h1>
+        <h1 class="text-primary text-5xl font-serif">winterhacks dec. 2023</h1>
+        <h2 class="text-primary text-4xl font-serif mt-2">coming soon</h2>
+        {#each snowflakes as snowflake}
+            <Snowflake {...snowflake} />
+        {/each}
     </div>
     <main>
-        <Navbar appearY={navbarAppearY} />
-        <div class="h-screen" id="about" />
+        <!-- <Navbar appearY={navbarAppearY} /> -->
+        <!-- <div class="h-screen" id="about" />
         <div class="h-screen" id="events" />
         <div class="h-screen" id="team" />
-        <div class="h-screen" id="contact" />
-        <div class="h-screen" />
-        <div class="h-screen" />
-        <div class="h-screen" />
-        <div class="h-screen" />
-        <div class="h-screen" />
+        <div class="h-screen" id="contact" /> -->
     </main>
 </div>
