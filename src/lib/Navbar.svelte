683ba01635ed24854409b4e538cf9fadf3c3a703
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2023-10-07 23:30:26 -0400

    fixed navbar styling

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,26 +1,24 @@
 <script lang="ts">
-    import { quadOut } from 'svelte/easing';
     import NavLink from './NavLink.svelte';
 
     let scrollY: number;
     let navHeight: number;
     let screenHeight: number;
     let isHovered = false;
-    $: navVisibility = quadOut(
+    $: navVisibility = (
         Math.min(1, Math.max(0, (screenHeight + navHeight - scrollY) / navHeight / 2))
     );
     $: navY = isHovered ? 0 : (1 - navVisibility) * -navHeight;
-    $: hidden = navVisibility <= 0;
 </script>
 
 <nav
-    class="w-screen pt-10 px-28 fixed top-0 z-10 transition-transform"
+    class="w-screen pt-10 px-28 fixed top-0 z-10"
     bind:clientHeight={navHeight}
     on:mouseenter={() => isHovered = true}
     on:mouseleave={() => isHovered = false}
 >
     <div
-        class="w-full bg-light h-24 rounded-md shadow-sm flex items-center justify-center gap-16 transition-[translate] duration-300 translate-y-0"
+        class="w-full bg-light h-24 rounded-md shadow-sm flex items-center justify-center gap-16 transition-[translate] duration-500"
         style="translate: 0 clamp(-{navHeight}px, {navY}px, 0rem)"
     >
         <NavLink href="#home" text="Home" />
