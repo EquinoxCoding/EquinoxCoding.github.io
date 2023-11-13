2447869f59feed802fe195efeaf61864aba20abc
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2023-11-13 01:19:35 -0500

    fixed logo

--- src/lib/assets/Logo.svelte
+++ src/lib/assets/Logo.svelte
@@ -1,57 +1,60 @@
 <script lang="ts">
-	export let color: string = "#385d8f"
-
+    export let color: string = '#385d8f';
 </script>
+
 <svg
     version="1.1"
-    id="Layer_1"
     xmlns="http://www.w3.org/2000/svg"
     xmlns:xlink="http://www.w3.org/1999/xlink"
     x="0px"
     y="0px"
-    viewBox="0 0 273.2 296.5"
-    style="enable-background:new 0 0 273.2 296.5; --color: {color};"
+    viewBox="0 0 300 300"
+    style="enable-background:new 0 0 300 300; --color: {color}"
     xml:space="preserve"
 >
     <style type="text/css">
-		.border {
+        .line {
+            stroke: var(--color);
+            stroke-width: 15;
+            stroke-linecap: round;
+            stroke-linejoin: round;
+        }
+        .border {
             fill: none;
             stroke: var(--color);
             stroke-width: 20;
             stroke-linecap: round;
             stroke-linejoin: round;
         }
-		.circle {
-			fill: var(--color);
-            stroke: var(--color);
-		}
-        .line {
+        .circle {
             fill: var(--color);
-            stroke: var(--color);
-            stroke-width: 15;
-            stroke-linecap: round;
-            stroke-linejoin: round;
         }
     </style>
-    <path
-        id="LogoBorder"
-        class="border"
-        d="M35.9,72.5l108-56.5l106.2,57l0,136.4l-104.7,62.1l-109.5-63V72.5z"
-    />
-    <line class="line" x1="141.2" y1="62.3" x2="141.2" y2="142.8" />
-    <line class="line" x1="122.8" y1="74.7" x2="140.1" y2="90.7" />
-    <line class="line" x1="140.1" y1="143.2" x2="83.6" y2="102.6" />
-    <line class="line" x1="93.6" y1="94.1" x2="95.3" y2="113.3" />
-    <line class="line" x1="76.8" y1="119.8" x2="95.2" y2="115.9" />
-    <line class="line" x1="141.1" y1="143.3" x2="141.8" y2="223.8" />
-    <line class="line" x1="140.1" y1="141.7" x2="86" y2="182.4" />
-    <line class="line" x1="93.5" y1="192" x2="95.3" y2="172.8" />
-    <line class="line" x1="76.8" y1="166.3" x2="95.2" y2="170.2" />
-    <circle class="circle" cx="202.7" cy="180.3" r="12" />
-    <path class="line" d="M201.5,177.7l-25.3-18.5l-16.3-0.6" />
-    <line class="line" x1="203.5" y1="152.3" x2="192.4" y2="164.8" />
-    <polyline class="line" points="182.6,126.5 182.6,114.5 200.2,101.3 " />
-    <circle class="circle" cx="200" cy="101.2" r="12" />
-    <line class="line" x1="159.6" y1="129.1" x2="205.4" y2="129.1" />
-    <line class="line" x1="123.2" y1="211.6" x2="140.6" y2="195.5" />
+    <g id="original">
+        <line class="line" x1="147.1" y1="146.4" x2="83.8" y2="105.8" />
+        <line class="line" x1="145.2" y1="150.7" x2="81.9" y2="191.4" />
+        <line class="line" x1="167.4" y1="81" x2="150" y2="97" />
+        <g>
+            <path
+                id="LogoBorder"
+                class="border"
+                d="M42.9,78.8l108-56.5l106.2,57l0,136.4l-104.7,62.1l-109.5-63V78.8z"
+            />
+            <line class="line" x1="148.2" y1="68.6" x2="148.2" y2="149.1" />
+            <line class="line" x1="129.8" y1="81" x2="147.1" y2="97" />
+            <line class="line" x1="100.5" y1="100.4" x2="102.3" y2="119.6" />
+            <line class="line" x1="83.8" y1="126.1" x2="102.2" y2="122.2" />
+            <line class="line" x1="148.1" y1="149.6" x2="148.8" y2="230.1" />
+            <line class="line" x1="100.5" y1="198.3" x2="102.3" y2="179.1" />
+            <line class="line" x1="83.8" y1="172.6" x2="102.2" y2="176.5" />
+            <circle class="circle" cx="209.7" cy="186.6" r="12" />
+            <path class="line" d="M208.5,184l-25.3-19.1h-16.3" />
+            <line class="line" x1="210.5" y1="158.6" x2="199.4" y2="171.1" />
+            <polyline class="line" points="189.6,132.8 189.6,120.8 207.2,107.6 " />
+            <circle class="circle" cx="207" cy="107.5" r="12" />
+            <line class="line" x1="166.6" y1="135.4" x2="212.4" y2="135.4" />
+            <line class="line" x1="130.2" y1="217.9" x2="147.6" y2="201.8" />
+        </g>
+        <line class="line" x1="167.4" y1="217.9" x2="150" y2="201.8" />
+    </g>
 </svg>
