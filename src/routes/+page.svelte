56718f80651f8bdc22e4b5131a0d1775a3d241ae
Andrew <67875282+andrewchu16@users.noreply.github.com>
2023-12-09 15:00:22 -0500

    apply button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,20 @@
+<script>
+    import ApplyButton from "$lib/components/ApplyButton.svelte";
+
+</script>
 <div class="h-screen bg-neutral relative" id="home">
-    <div class="absolute bottom-20 left-28">
-        <h1 class="text-9xl text-dark-700 drop-shadow-xl mb-1">Winter Hacks</h1>
-        <h2 class="text-4xl text-dark-500 drop-shadow-xl">Jan 12-13, 2024 (virtual!)</h2>
+    <div class="absolute bottom-[8vh] left-[4vw]">
+        <h1 class="text-8xl text-dark-700 drop-shadow-xl mb-1">Winter Hacks</h1>
+        <h2 class="text-4xl text-dark-500 drop-shadow-xl mb-5">Jan 12-13, 2024 (virtual!)</h2>
+        <ApplyButton href="https://forms.gle/c1NMhJPwZs59Dg72A" text="Apply Now"/>
+        <ApplyButton href="https://forms.gle/u93369rR9Xh3qXYw5" text="Become a Volunteer" />
     </div>
 </div>
 <div class="py-24 bg-neutral" id="about">
-    <div class="ml-32 bg-light h-[550px] w-[60%] rounded-lg drop-shadow-xl">
-
-    </div>
+    <div class="ml-32 bg-light h-[550px] w-[60%] rounded-lg drop-shadow-xl" />
 </div>
-<div class="h-screen bg-dark-300" id="faq">
-    faq
-</div>
+<div class="h-screen bg-dark-300" id="faq">faq</div>
 <!-- <div class="h-screen bg-dark-300" id="sponsors">
     sponsors
 </div> -->
-<div class="h-screen bg-dark-500" id="team">
-    team
-</div>
+<div class="h-screen bg-dark-500" id="team">team</div>
