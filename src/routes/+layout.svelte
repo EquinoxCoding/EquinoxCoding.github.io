494ba231d9931022a53de262cbd78398bbc48d32
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2023-12-09 15:14:55 -0500

    meta data

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,18 @@
-<script>
+<script lang="ts">
     import Navbar from '$lib/components/Navbar.svelte';
     import '../app.css';
+
+    const TITLE = "WinterHacks";
+    const DESCRIPTION = "WinterHacks: virtually on January 12-13, 2024. Come join Equinox Coding's newest seasonal hackathon made by students, for students.";
 </script>
+
+<svelte:head>
+    <title>{TITLE}</title>
+    <meta property="og:title" content={TITLE} />
+    <meta name="description" content={DESCRIPTION} />
+    <meta property="og:description" content={DESCRIPTION} />
+    <meta property="og:type" content="website">
+</svelte:head>
 
 <Navbar />
 <slot />
